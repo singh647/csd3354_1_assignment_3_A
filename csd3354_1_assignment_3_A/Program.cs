792887c10c31974eefdc52b2d75b7e69942a700c
visualstudio <visualstudio@230-PC39>
2019-03-14 15:33:04 -0400

    bank account

--- csd3354_1_assignment_3_A/Program.cs
+++ csd3354_1_assignment_3_A/Program.cs
@@ -5,11 +5,73 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace csd3354_1_assignment_3_A
+//rupinderpal singh c0723889
+//ramandeep singh brar c0730408
 {
     class Program
     {
-        static void Main(string[] args)
+
+    }
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+        private bool m_frozen = false;
+        private BankAccount()
         {
         }
+        public BankAccount(String customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+
+        }
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+        public void Debit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+        public void Credit(double amount)
+        {
+            if (m_frozen)
+            {
+                throw new Exception("Account frozen");
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+            m_balance += amount;
+        }
+
+
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is $ {0}", ba.Balance);
+        }
     }
+
 }
